768b7b137e7b37e0731e94bd017b3c03d1c45030
Dirk Watkins <dirk.watkins@perficient.com>
2023-02-22 10:46:17 -0600

    Add project files.

---
+++ Receiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using Receiver;
+using System.IO.Ports;
+
+Console.WriteLine("RECEIVER");
+
+
+Console.WriteLine("Available Ports:");
+foreach (string s in SerialPort.GetPortNames())
+{
+    Console.WriteLine("   {0}", s);
+}
+
+var serialPort = new SerialPort("COM2", 9600);
+serialPort.DataReceived += DataHandler.DataReceived;
+
+
+try
+{
+    if (!serialPort.IsOpen)
+        serialPort.Open();
+
+}
+catch (UnauthorizedAccessException ex)
+{
+    Console.WriteLine("Error: Port {0} is in use", serialPort.PortName);
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex);
+}
+
+
+Console.ReadLine();
---
+++ Sender/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.IO.Ports;
+
+Console.WriteLine("SENDER");
+
+//Console.WriteLine("Available Ports:");
+//foreach (string s in SerialPort.GetPortNames())
+//{
+//    Console.WriteLine("   {0}", s);
+//}
+
+//wait for the listener to start
+Thread.Sleep(500);
+
+var serialPort = new SerialPort("COM1", 9600);
+
+
+try
+{
+    if (!serialPort.IsOpen)
+        serialPort.Open();
+
+    Console.WriteLine("I'm sending a message!");
+
+    serialPort.WriteLine("did you see this?");
+
+    Thread.Sleep(500);
+    serialPort.WriteLine("how about this?");
+
+    Thread.Sleep(500);
+    serialPort.WriteLine("and this?");
+
+    Thread.Sleep(500);
+    serialPort.WriteLine("that's all I have to say.");
+
+}
+catch (UnauthorizedAccessException ex)
+{
+    Console.WriteLine("Error: Port {0} is in use", serialPort.PortName);
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex);
+
+}
+
+Console.ReadLine();
